08a9e40fced4e941dc43275acc0ac7c2743a7692
Jimmy <noreply@noreply.com>
2020-10-28 20:54:06 +0100

    optimize plus start test classes

--- poarestservice/src/test/java/nl/rabobank/powerofattorney/application/service/UserServiceTest.java
+++ poarestservice/src/test/java/nl/rabobank/powerofattorney/application/service/UserServiceTest.java
@@ -2,10 +2,12 @@ package nl.rabobank.powerofattorney.application.service;
 
 import nl.rabobank.powerofattorney.application.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Autowired
